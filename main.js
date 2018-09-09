e4157c7cace1dd275116770618b646a0eb07ef0e
Kate Dameron <katerj33@gmail.com>
2018-09-09 10:58:36 -0700

    guest order prototype added to push orders to array

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-function UserAccount(username, password, first, last, phone, address) {
+function UserAccount(username, password, first, last, phone, address, bread, spreads, jams, toppings, cut) {
     this.username = username;
     this.password = password;
     this.lastName = last;
     this.firstName = first;
     this.phone = phone;
     this.address = address;
-    //below adds one sandwich but when you try to add another it overrides the first
-    this.addSandwich = function(bread, spreads, jams, toppings, cut) {
-        this.bread = bread;
-        this.spreads = spreads;
-        this.jams = jams;
-        this.toppings = toppings
-        this.cut = cut;
-    } 
-}
+    
+    this.addSandwich = [];
+};
 //array to store user account objects
 var accounts = [];
-var faveSandwiches = [];
+
+function Sandwich(bread, spreads, jams, toppings, cut) {
+    this.bread = bread;
+    this.spreads = spreads;
+    this.jams = jams;
+    this.toppings = toppings
+    this.cut = cut;
+}
+
+UserAccount.prototype.addFaveSandwich = function(sandwich) {
+    addSandwich.push(new Sandwich(sandwich));
+} 
+    
+UserAccount.prototype.addAccount = function(profile) {
+    accounts.push(new UserAccount(profile));
+}
+
+accounts.push(new UserAccount("sarahbean", "coolbeans", "Bean", "Sarah", "503-555-1234", "34 N Sandwich Ave"));
 
 //use this constructor to store temp data to guestOrderSum array
 function GuestOrder(bread, spreads, jams, toppings, cut) {
@@ -28,3 +39,4 @@ function GuestOrder(bread, spreads, jams, toppings, cut) {
 }
 
 var guestOrderSum = [];
+

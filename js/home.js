502ddc6a4993058fd70b2c8e26b0c0a46a51d29e
Kate Dameron <katerj33@gmail.com>
2018-09-09 15:22:36 -0700

    first commit

---
+++ js/home.js
@@ -0,0 +1,14 @@
+var getRegistrationButton = document.getElementById('registerButton');
+addUserAccount = function() {
+    var username = document.getElementById('register-username').value;
+    var password = document.getElementById('register-password').value;
+    var lastName = document.getElementById('register-lastName').value;
+    var firstName = document.getElementById('register-firstName').value;
+    var phone = document.getElementById('register-phone').value;
+    var address = document.getElementById('register-address').value;
+    accounts.push(new UserAccount(username, password, firstName, lastName, phone, address));
+    //call add to local storage function
+}
+getRegistrationButton.addEventListener("click", addUserAccount);
+
+var getLoginButton = document.getElementById('');

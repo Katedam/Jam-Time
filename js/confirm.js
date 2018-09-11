1753b013a09e637dc187dd4d8fd5d5e4e282b998
Zachar13y <zachar13y@gmail.com>
2018-09-11 14:32:09 -0700

    Added showAddress function to check local storage.

--- js/confirm.js
+++ js/confirm.js
@@ -1,13 +1,19 @@
 console.log('confirm.js is plugged in'); //Verifying that this file is being read.
 
-JSON.parse(window.localStorage.getItem('Address'));
 
 function showAddress() {
-    var Address = document.getElementById('user-place');
-    document.appendChild
+    var address = JSON.parse(localStorage.getElementById('user-place'));
+        if (address != null) {
+        var giveAddress = document.createElement('h5');
+        giveAddress.innerText = address.value;
+        giveAddress.appendChild(address);
+        }
 }
 
+window.addEventListener('load', showAddress);
 console.log('Address');
+
+
 
 
 
